d4bc673a653c01c11157ae77663cd5715bfafaed
Ethan Girouard <mr.egirouard@gmail.com>
2022-12-01 23:23:09 -0500

    Add Discord.js client login

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
 import log from './log'
+import dotenv from 'dotenv'
+import discord from 'discord.js'
 
 log.info('Starting StreakCode bot...')
+
+dotenv.config()
+
+log.debug('Creating Discord client...')
+const dClient = new discord.Client({ intents: [] })
+
+log.verbose('Logging in to Discord...')
+dClient.login(process.env.DISCORD_TOKEN)
+
+dClient.on('ready', () =>
+{
+	log.info('StreakCode bot is ready!')
+})
