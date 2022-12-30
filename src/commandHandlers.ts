680e371cf8bdb17a46c86f0922ff45699136d47a
Ethan Girouard <mr.egirouard@gmail.com>
2022-12-30 18:03:59 -0500

    Add ping command handler

--- src/commandHandlers.ts
+++ src/commandHandlers.ts
@@ -1,6 +1,15 @@
 import discord from 'discord.js'
+import log from './log'
 
 type Reply = string | discord.InteractionReplyOptions | discord.MessagePayload
 let handlers: Record<string, (interaction: discord.CommandInteraction) => Promise<Reply>> = {}
+
+handlers['ping'] = (interaction: discord.CommandInteraction): Promise<Reply> =>
+{
+	log.debug('Ping')
+	return new Promise((resolve, reject) => resolve('Pong!\n' +
+	`Websocket Heartbeat: ${interaction.client.ws.ping}ms\n` +
+	`Roundtrip Latency: ${Date.now() - interaction.createdTimestamp}ms`))
+}
 
 export default handlers
